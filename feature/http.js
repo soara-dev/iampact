47334eaf43a624151f698265f8579b45f77dad98
Harithya Wisesa <harithya77@gmail.com>
2024-12-26 20:10:00 +0700

    add param onBeforeSend + onComplete

--- feature/http.js
+++ feature/http.js
@@ -3,6 +3,8 @@ class Http {
     this.baseUrl = config.baseUrl || "";
     this.onSuccess = config.onSuccess || (() => {});
     this.onError = config.onError || (() => {});
+    this.onBeforeSend = config.onBeforeSend || (() => {});
+    this.onComplete = config.onComplete || (() => {});
   }
 
   create(param) {
@@ -10,18 +12,23 @@ class Http {
       baseUrl: param.baseUrl,
       onSuccess: param.onSuccess,
       onError: param.onError,
+      onBeforeSend: param.onBeforeSend,
+      onComplete: param.onComplete,
     });
   }
 
   request(param) {
-    const { url, method, data, onSuccess, onError } = param;
+    const { url, method, data, onSuccess, onError, onBeforeSend, onComplete } =
+      param;
 
     const requestConfig = {
       url: this.baseUrl ? this.baseUrl + url : url,
       method: method || "GET",
       data: data || {},
+      onBeforeSend: onBeforeSend || this.onBeforeSend || (() => {}),
       onSuccess: onSuccess || this.onSuccess || (() => {}),
       onError: onError || this.onError || (() => {}),
+      onComplete: onComplete || this.onComplete || (() => {}),
     };
 
     return $.ajax({
@@ -31,11 +38,17 @@ class Http {
       contentType: false,
       processData: false,
       async: false,
+      beforeSend: function (xhr) {
+        requestConfig.onBeforeSend(xhr);
+      },
       success: function (res) {
         requestConfig.onSuccess(res);
       },
       error: function (err) {
         requestConfig.onError(err);
+      },
+      complete: function () {
+        requestConfig.onComplete();
       },
     });
   }
