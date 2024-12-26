e746a8f27b64752ba727003dc0a5fdac4c642338
fikriwado <mfikri.khoirurrizal.com>
2024-12-26 18:02:50 +0700

    create build for production and change watch to dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
-const { watch } = require("fs");
 const path = require("path");
 
-module.exports = {
-  entry: "./main.js",
-  mode: "development",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
+module.exports = (env, argv) => {
+  return {
+    entry: "./main.js",
+    mode: argv.mode || "development",
+    output: {
+      filename: "main.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    devtool:
+      argv.mode === "production"
+        ? "source-map"
+        : "eval-cheap-module-source-map",
+  };
 };
