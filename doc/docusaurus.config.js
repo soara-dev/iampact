93b1932ed3b0a6bc18d1b109350f728b8057f8c8
fikriwado <mfikri.khoirurrizal.com>
2024-12-27 21:16:31 +0700

    change base url

--- doc/docusaurus.config.js
+++ doc/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: "https://iam-starterpack.iamevent.my.id/blog",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/doc/build",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
