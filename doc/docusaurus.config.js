dad73481dba18c0029e0134a0fff32a2ebb927c2
fikriwado <mfikri.khoirurrizal.com>
2024-12-27 21:12:13 +0700

    add docusaurus with auto build

--- doc/docusaurus.config.js
+++ doc/docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "http://localhost:8000",
+  url: "https://iam-starterpack.iamevent.my.id/blog",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/doc/build",
