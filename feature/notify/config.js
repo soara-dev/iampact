38773097cfb365781f6ff37c9a52e23c6f0b144a
fikriwado <mfikri.khoirurrizal.com>
2024-12-28 11:56:43 +0700

    feat: create notify utility

---
+++ feature/notify/config.js
@@ -0,0 +1,31 @@
+export const global = {
+  timer: 3000,
+  message: {
+    error: "Oops! Something went wrong",
+    success: "Action performed successfully",
+    warning: "This action needs attention",
+  },
+};
+
+export const globalToast = {
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.onmouseenter = Swal.stopTimer;
+    toast.onmouseleave = Swal.resumeTimer;
+  },
+};
+
+export const globalSwal = {};
+
+export const globalConfirm = {
+  title: "Are you sure?",
+  text: "You won't be able to revert this!",
+  icon: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#3085d6",
+  cancelButtonColor: "#d33",
+  confirmButtonText: "Yes, delete it!",
+};
--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-import validate from "./feature/validate";
 import httpInstance from "./feature/http";
+import notify from "./feature/notify/notify";
+import validate from "./feature/validate";
 
 const iam = {
   create: httpInstance.create,
   request: httpInstance.request,
   utils: {
+    notify,
     validate,
   },
 };
