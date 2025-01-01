0dd5c2291299845c10486d1b28c55c29cde105aa
fikriwado <mfikri.khoirurrizal.com>
2025-01-01 13:06:44 +0700

    remove unused code and assets

--- doc/src/pages/index.jsx
+++ doc/src/pages/index.jsx
@@ -1,20 +1,12 @@
-import clsx from "clsx";
 import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
-
 import Navbar from "@theme/Navbar";
-import Heading from "@theme/Heading";
-import styles from "./index.module.css";
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       noNavbar
       noFooter
-      title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />"
     >
       <main>
