2b55f529fb221b1ca1b5271e2be589c78a410ac4
Harithya Wisesa <harithya77@gmail.com>
2025-02-04 13:44:46 +0700

    add feature callback in fillform

--- feature/http/fillForm.js
+++ feature/http/fillForm.js
@@ -1,7 +1,10 @@
 export function fillForm(data, param = {}) {
-    const { except } = param;
+    const { except, callback } = param;
     const keys = Object.keys(data).filter(key => !except?.includes(key));
     keys.forEach(key => {
         $(`[name=${key}]`).val(data[key]).trigger('change');
     });
+    setTimeout(() => {
+        if (callback) callback();
+    }, 300);
 }
